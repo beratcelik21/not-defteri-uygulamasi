f0e71314306c2af0415270b6917c0b721a24ef3b
Berat Celik <beratcelik198007@gmail.com>
2024-09-20 23:11:06 +0200

    Categoryjs

---
+++ not-defteri-uygulamasi/backend/models/Category.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
